723c2485d296a4e67ad5244cbcc141061f32cfd0
patfre <56884994+patfr@users.noreply.github.com>
2022-02-08 11:18:43 +0000

    v0.001

--- js/layers.js
+++ js/layers.js
@@ -1,28 +1,93 @@
-addLayer("p", {
-    name: "prestige", // This is optional, only used in a few places, If absent it just uses the layer id.
-    symbol: "P", // This appears on the layer's node. Default is the id with the first letter capitalized
-    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+addLayer("c", {
+    name: "Coins",
+    symbol: "C",
+	resource: "Coins",
+    position: 0,
     startData() { return {
         unlocked: true,
 		points: new Decimal(0),
+		second: new Decimal(0),
+
+		workers: { count: new Decimal(0), amount: new Decimal(0), second: new Decimal(0) },
+		investments: { count: new Decimal(0), amount: new Decimal(0), second: new Decimal(0) },
     }},
-    color: "#4BDC13",
-    requires: new Decimal(10), // Can be a function that takes requirement increases into account
-    resource: "prestige points", // Name of prestige currency
-    baseResource: "points", // Name of resource prestige is based on
-    baseAmount() {return player.points}, // Get the current amount of baseResource
-    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-    exponent: 0.5, // Prestige currency exponent
-    gainMult() { // Calculate the multiplier for main currency from bonuses
-        mult = new Decimal(1)
-        return mult
-    },
-    gainExp() { // Calculate the exponent on main currency from bonuses
-        return new Decimal(1)
-    },
-    row: 0, // Row the layer is in on the tree (0 is the first row)
-    hotkeys: [
-        {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
-    ],
-    layerShown(){return true}
+    color: "gold",
+    type: "none",
+	displayRow: 0,
+    row: 0,
+    layerShown(){ return true },
+	update() {
+		player[this.layer].points = player.points
+
+		let workers = player[this.layer].workers.count.add(player[this.layer].workers.amount).mul(10)
+		let investments = player[this.layer].investments.count.add(player[this.layer].investments.amount).mul(100)
+		
+		player[this.layer].workers.second = workers
+		player[this.layer].investments.second = investments
+
+		let total = workers.add(investments)
+
+		player[this.layer].second = total
+	},
+	effect() { return player[this.layer].second },
+	tabFormat: [
+		function() {
+			return ["display-text", `You have <h2 style='color:gold;text-shadow:#ffff00 0px 0px 10px'>${format(player.points)}</h2> Coins`]
+		},
+		["blank", "25px"],
+		"h-line",
+		["blank", "25px"],
+		function() {
+			return ['column', [
+				["row", [
+					["raw-html", `<div style='width:250px;text-align:left'>Workers: ${format(player[this.layer].workers.count)} [+${format(player[this.layer].workers.amount)}]</div>`, {"color":"gold"}],
+					["blank", ["150px", "10px"]],
+					["buyable", "11"],
+					["raw-html", `<div style='width:250px;text-align:right'>Coins/Sec: ${format(player[this.layer].workers.second)} [${format(player[this.layer].workers.second.div(player[this.layer].second.max(1)).mul(100).min(100).max(0))}%]</div>`, {"color":"gold"}],
+				]],
+				["row", [
+					["raw-html", `<div style='width:250px;text-align:left'>Investments: ${format(player[this.layer].investments.count)} [+${format(player[this.layer].investments.amount)}]</div>`, {"color":"gold"}],
+					["blank", ["150px", "10px"]],
+					["buyable", "12"],
+					["raw-html", `<div style='width:250px;text-align:right'>Coins/Sec: ${format(player[this.layer].investments.second)} [${format(player[this.layer].investments.second.div(player[this.layer].second.max(1)).mul(100).min(100).max(0))}%]</div>`, {"color":"gold"}],
+				]],
+			]]
+		},
+	],
+	buyables: {
+		11: {
+			cost() { return new Decimal(1.26).pow(player[this.layer].workers.count).mul(100) },
+			display() { return `Cost: ${format(this.cost())} Coins` },
+			canAfford() { return player.points.gte(this.cost()) },
+			buy() {
+				player.points = player.points.sub(this.cost())
+				player[this.layer].workers.count = player[this.layer].workers.count.add(1)
+			},
+			style() {
+				let s = {};
+				if (this.canAfford()) s["background-color"] = "#666"; else s["background-color"] = "transparent"
+				s["width"] = "150px"
+				s["height"] = "30px"
+				s["color"] = "white"
+				return s
+			},
+		},
+		12: {
+			cost() { return new Decimal(1.563).pow(player[this.layer].investments.count).mul(2000) },
+			display() { return `Cost: ${format(this.cost())} Coins` },
+			canAfford() { return player.points.gte(this.cost()) },
+			buy() {
+				player.points = player.points.sub(this.cost())
+				player[this.layer].investments.count = player[this.layer].investments.count.add(1)
+			},
+			style() {
+				let s = {};
+				if (this.canAfford()) s["background-color"] = "#666"; else s["background-color"] = "transparent"
+				s["width"] = "150px"
+				s["height"] = "30px"
+				s["color"] = "white"
+				return s
+			},
+		},
+	},
 })
--- js/mod.js
+++ js/mod.js
@@ -1,79 +1,74 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
-	pointsName: "points",
+	name: "The Synergism Tree",
+	id: "TheSynergismTree",
+	author: "patfr",
+	pointsName: "Coins",
 	modFiles: ["layers.js", "tree.js"],
 
 	discordName: "",
 	discordLink: "",
-	initialStartPoints: new Decimal (10), // Used for hard resets and new players
-	offlineLimit: 1,  // In hours
+	initialStartPoints: new Decimal (100),
+	offlineLimit: 0,
 }
 
-// Set your version in num and name
 let VERSION = {
-	num: "0.0",
-	name: "Literally nothing",
+	num: "0.001",
+	name: "Initial release",
 }
 
-let changelog = `<h1>Changelog:</h1><br>
-	<h3>v0.0</h3><br>
-		- Added things.<br>
-		- Added stuff.`
+let changelog = `
+<h3>Inspired by: <a href='https://pseudo-corp.github.io/SynergismOfficial/'>Synergism</a></h3>
+<br><br><br>
+<h1>Version info:</h1><br><br>
+	<h3>vA.B.C</h3><br>
+		- A: Major update<br>
+		- B: Small update<br>
+		- C: Minor fixes
+<br><br><br>
+<h1>Changelog:</h1><br><br>
+	<h3>v0.001</h3><br>
+		- Added 2 buyables.<br>
+		- Added coins.`
 
 let winText = `Congratulations! You have reached the end and beaten this game, but for now...`
 
-// If you add new functions anywhere inside of a layer, and those functions have an effect when called, add them here.
-// (The ones here are examples, all official functions are already taken care of)
 var doNotCallTheseFunctionsEveryTick = ["blowUpEverything"]
 
 function getStartPoints(){
     return new Decimal(modInfo.initialStartPoints)
 }
 
-// Determines if it should show points/sec
 function canGenPoints(){
 	return true
 }
 
-// Calculate points/sec!
 function getPointGen() {
 	if(!canGenPoints())
 		return new Decimal(0)
 
-	let gain = new Decimal(1)
+	let gain = new Decimal(0)
+	gain = gain.add(tmp.c.effect)
 	return gain
 }
 
-// You can add non-layer related variables that should to into "player" and be saved here, along with default values
 function addedPlayerData() { return {
 }}
 
-// Display extra things at the top of the page
 var displayThings = [
 ]
 
-// Determines when the game "ends"
 function isEndgame() {
 	return player.points.gte(new Decimal("e280000000"))
 }
 
-
-
-// Less important things beyond this point!
-
-// Style for the background, can be a function
 var backgroundStyle = {
 
 }
 
-// You can change this if you have things that can be messed up by long tick lengths
 function maxTickLength() {
-	return(3600) // Default is 1 hour which is just arbitrarily large
+	return(3600)
 }
 
-// Use this if you need to undo inflation from an older version. If the version is older than the version that fixed the issue,
-// you can cap their current resources with this.
-function fixOldSave(oldVersion){
+function fixOldSave(oldVersion) {
+
 }
